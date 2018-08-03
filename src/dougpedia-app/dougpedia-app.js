239072d9cab493595232d3a5efee278d15f2eeb5
Mario Aleo <marioaleo@msn.com>
2018-08-03 17:54:23 -0300

    Creating the card lifecycle with swipe and animations

--- src/dougpedia-app/dougpedia-app.js
+++ src/dougpedia-app/dougpedia-app.js
@@ -8,6 +8,8 @@ import 'firebase/database';
 import '@material/mwc-fab';
 import '@material/mwc-icon';
 import '@material/mwc-button';
+import 'dougpedia-joke-card';
+import '@polymer/iron-swipeable-container';
 
 firebase.initializeApp({
   apiKey: "AIzaSyCsm-Pyr1cW-BOf8po98j0nEMH164X-8Z8",
@@ -28,6 +30,7 @@ class DougpediaApp extends connect(store)(LitElement) {
       _offline: Boolean,
       _jokeList: Array,
       _authorized: Boolean,
+      _activeJokeIndex: Number
     };
   }
 
@@ -118,9 +121,9 @@ class DougpediaApp extends connect(store)(LitElement) {
 
         <mwc-icon id="network-status">
           ${this._offline
-        ? html`cloud_off`
-        : html`cloud_queue`
-      }
+            ? html`cloud_off`
+            : html`cloud_queue`
+          }
         </mwc-icon>
       </section>
     `;
@@ -162,12 +165,18 @@ class DougpediaApp extends connect(store)(LitElement) {
       <style>
         #content {
           grid-area: appContent;
+          transition: opacity ease 0.25s, visibility ease 0.25s;
+          will-change: opacity, transition, visibility;
+        }
+        #content:not([authorized]) {
+          opacity: 0;
+          visibility: hidden;
         }
 
         #add {
           position: fixed;
-          bottom: 32px;
-          right: 32px;
+          bottom: 16px;
+          right: 16px;
           transition: opacity ease 0.25s, visibility ease 0.25s;
           will-change: opacity, transition, visibility;
         }
@@ -175,16 +184,18 @@ class DougpediaApp extends connect(store)(LitElement) {
           opacity: 0;
           visibility: hidden;
         }
+
+        dougpedia-joke-card {
+          padding: 32px 16px;
+        }
       </style>
 
-      <section id="content">
-        ${this._jokeList.map(joke =>
-          html`
-            <dougpedia-joke-card
-              key$="${joke}">
-            </dougpedia-joke-card>
-          `
-        )}
+      <section id="content"
+        authorized?="${this._authorized}">
+        <iron-swipeable-container disabled?=${!this._jokeList[this._activeJoke]}
+          on-iron-swipe="${this._jokeDismissed.bind(this)}">
+          <dougpedia-joke-card></dougpedia-joke-card>
+        </iron-swipeable-container>
       </section>
 
       <mwc-fab id="add"
@@ -217,29 +228,68 @@ class DougpediaApp extends connect(store)(LitElement) {
 
   /* Private */
   _loadJokeList() {
-    /*
-    firebase.database()
-      .ref('/jokes')
-      .push({
-        uid: 'momo',
-        text: 'joke',
-        rating: [
-          {uid: 'lala', rate: 5}
-        ]
-      })
-      .then(res => console.log(res))
-      .catch(err => console.error(err));
-    */
     firebase.database()
       .ref(`/jokes`)
-      .on('value', snapshot =>
+      .on('value', snapshot => {
         store.dispatch({
           type: 'UPDATE_JOKE_LIST',
           data: {
             jokeList: Object.keys(snapshot.val())
           }
+        });
+        this._activeJoke = 0;
+        this._setJokeCardKey();
+    });
+  }
+
+  _jokeDismissed(evt) {
+    if (evt.detail.direction == 'left') {
+      if (this._activeJoke == this._jokeList.length - 1)
+        this._activeJoke = 0;
+      else
+        this._activeJoke++;
+    } else {
+      if (this._activeJoke == 0)
+        this._activeJoke = this._jokeList.length - 1;
+      else
+        this._activeJoke--;
+    }
+    this._appendJokeCard(evt.detail.direction)
+      .then(
+        this._setJokeCardKey.bind(this)
+      );
+  }
+
+  _appendJokeCard(direction) {
+    return new Promise(resolve => {
+      const jokeCard = document.createElement('dougpedia-joke-card');
+      jokeCard.setAttribute('key', '');
+      Object.assign(jokeCard.style, {
+        transform: `
+          translateX(${direction == 'left' ? '100%': '-100%'})
+        `
+      });
+      this.shadowRoot.querySelector('iron-swipeable-container')
+        .appendChild(jokeCard);
+      requestAnimationFrame(() => 
+        requestAnimationFrame(() => {
+          Object.assign(jokeCard.style, {
+            transform: `translateX(0)`
+          });
+          jokeCard.addEventListener(
+            "transitionend",
+            () => setTimeout(resolve, 200),
+            false
+          );
         })
       );
+    });
+  }
+
+  _setJokeCardKey() {
+    this.shadowRoot
+      .querySelector('dougpedia-joke-card')
+      .setAttribute('key', this._jokeList[this._activeJoke]);
   }
   /* */
 
